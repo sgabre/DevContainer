fd171c033a60d44231ce71333b2e071fd6499c94
sgabre <godefroy.sieffert>
2025-02-27 19:13:11 +0100

    Update

---
+++ src/main.c
@@ -0,0 +1,31 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(void)
+{
+
+
+
+printf("Hello, Dev Container (Basic)\n");
+
+}
+
